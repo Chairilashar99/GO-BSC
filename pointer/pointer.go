013647745fefddee495fd72c28aceedca53ed6de
chairil ashar <asharchairil4@gmail.com>
2023-06-14 10:44:02 +0700

    Pointer pass by value & reference pada variable

---
+++ pointer/pointer.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+type Address struct {
+	City, Province, Country string
+}
+
+func main() {
+	address1 := Address{"Jakarta", "DKI Jakarta", "Indonesia"}
+	//Pass by Value maksdnya mengcopy data pertama tanpa harus mengubah datanya
+	// address2 := address1
+
+	//Pass By reference maksudnya merubah ke dua datanya
+	address2 := &address1
+	address3 := &address1
+
+	address2.City = "Bandung"
+
+	*address2 = Address{
+		"Makassar", "Sulawesi", "Indonesia",
+	}
+
+	fmt.Println(address1)
+	fmt.Println(address2)
+	fmt.Println(address3)
+
+	// var address4 *Address = new(Address)
+	// fmt.Println(address4)
+
+	address4 := new(Address)
+	address4.City = "Soppeng"
+	fmt.Println(address4)
+}
